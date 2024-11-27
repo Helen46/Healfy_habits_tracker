22a638c2893ba86752e21d893740a1b020895586
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-11-27 09:45:02 +0300

    add permission

--- habits/views.py
+++ habits/views.py
@@ -1,7 +1,9 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from habits.models import Habit
 from habits.serializers import HabitSerializer
+from users.permissions import IsOwner
 
 
 class HabitViewSet(ModelViewSet):
@@ -12,3 +14,10 @@ class HabitViewSet(ModelViewSet):
     def perform_create(self, serializer):
        habit = serializer.save(owner=self.request.user)
        habit.save()
+
+    def get_permissions(self):
+        if self.action == "list" and Habit.objects.filter(is_published=True):
+            return self.permission_classes == (IsAuthenticated,)
+        else:
+            return self.permission_classes == (IsOwner,)
+
