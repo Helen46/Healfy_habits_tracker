f5cc1d1b879402131e1e3d6c2e188d4e8aa1d9c8
Elena Prilutskay <a.priluckay87@gmail.com>
2024-11-29 22:02:59 +0300

    add pagination

--- habits/views.py
+++ habits/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from habits.models import Habit
+from habits.pagination import HabitsPagination
 from habits.serializers import HabitSerializer
 from users.permissions import IsOwner
 
@@ -11,7 +12,8 @@ class HabitViewSet(ModelViewSet):
     """CRUD для привычек"""
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = (IsAuthenticated,)
+    pagination_class = HabitsPagination
 
     def perform_create(self, serializer):
        habit = serializer.save(owner=self.request.user)
@@ -19,7 +21,7 @@ class HabitViewSet(ModelViewSet):
 
     def get_permissions(self):
        if self.action != "create":
-           self.permission_classes = [IsAuthenticated, IsOwner]
+           self.permission_classes = (IsAuthenticated, IsOwner)
        return super().get_permissions()
 
 
@@ -30,3 +32,4 @@ class PublishedHabitListAPIView(generics.ListAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.filter(is_published=True)
     permission_classes = [IsAuthenticated]
+    pagination_class = HabitsPagination
